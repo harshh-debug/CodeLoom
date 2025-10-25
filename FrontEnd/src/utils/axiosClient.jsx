ab2c8cca91139864fbb65e5a1686090ad9ea8f39
harshh-debug <harshj9705@gmail.com>
2025-10-25 14:24:03 +0530

    fixed backend url

--- FrontEnd/src/utils/axiosClient.jsx
+++ FrontEnd/src/utils/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+const BackendUrl= import.meta.env.VITE_BACKEND_URL;
 const axiosClient = axios.create({
-    baseURL:"https://codeloom-backend-3pj7.onrender.com",
+    baseURL:BackendUrl|| 'http://localhost:3000',
     withCredentials:true,
     headers:{
         'Content-Type':'application/json'
