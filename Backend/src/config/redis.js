9a169ee5ae1fbf62b5bdd99bd5f7db17282f713f
Harsh Joshi <harshcode0907@gmail.com>
2026-03-17 22:49:41 +0530

    fixed redis client

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -4,8 +4,8 @@ import { createClient } from 'redis';
 export const redisClient = createClient({
     password: process.env.REDIS_PASSWORD,
     socket: {
-        host: process.env.REDIS_HOST || 'redis-16647.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: parseInt(process.env.REDIS_PORT) || 16647
+        host: process.env.REDIS_HOST ,
+        port: parseInt(process.env.REDIS_PORT) 
     }
 });
 
